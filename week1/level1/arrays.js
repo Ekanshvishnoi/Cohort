fc241979769d6467301ed3021c635f2ef7f46878
Ekansh Vishnoi <ekanshvishnoi6@gmail.com>
2025-08-05 20:15:29 +0530

    learning about arrays and numbers  in js

---
+++ week1/level1/arrays.js
@@ -0,0 +1,14 @@
+let arr=[1,12,41,42];
+let arr1=[32,4,35,5,23];
+// arr.push(32);
+// arr.pop();
+// arr.shift();
+// arr.unshift(83);
+// console.log(arr);
+// console.log(arr.concat(arr1));
+
+// function am(str){
+//   console.log(str);
+// }
+// arr.forEach(am);
+
---
+++ week1/level1/number.js
@@ -0,0 +1,11 @@
+// console.log(parseInt("42"));
+// console.log(parseInt("42jbsdvv"));
+// console.log(parseInt("42.297"));
+
+// // parse into float
+
+// console.log(parseFloat("42.297"));
+// console.log(parseFloat("42bfwuo"));
+
+
+
